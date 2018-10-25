77f6601e643034839cfa4a63947e772def17e8c0
Chris Kalahiki <chris.kalahiki@gmail.com>
2018-10-25 13:31:19 -0500

    Updated scratch work with comments

--- scratchwork.m
+++ scratchwork.m
@@ -6,9 +6,6 @@ dcm_dir = 'dicom_dir/';
 imagefiles = dir('dicom_dir/*.dcm');
 nfiles = length(imagefiles);
 
-%Create empty 100 x 17 matrix
-%img_arr = zeros(100,17);
-
 %For each *.dcm file in the dataset, load it and add it to the images array
 for inum=1:nfiles
     curr_name = imagefiles(inum).name;
@@ -17,21 +14,17 @@ for inum=1:nfiles
 end
 %images array now contains the entire dataset of dicom files
 
-
-
-%[V,spatial,dim] = dicomreadVolume(dcm_dir);
-
-%i=dicomread('image.dcm');
-
 sX=size(images{1});
+
 %Splits image into 4 layers
 [cA,cH,cV,cD]=dwt2(images{1},'db1');
+
 figure(1)
 subplot(2,2,1);imshow(cA);title('cA band of image');
 subplot(2,2,2);imshow(cH);title('cH band of image');
 subplot(2,2,3);imshow(cV);title('cV band of image');
 subplot(2,2,4);imshow(cD);title('cD band of image');
 X = idwt2(cA,cH,cV,cD,'db1',sX);
+
 figure(2)
-%Displays each layer
 imshow(X);
