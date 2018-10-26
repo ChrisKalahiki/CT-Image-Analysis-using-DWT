885fdf5bbe79990f7433294fb0e2cc600a0b9525
Chris Kalahiki <chris.kalahiki@gmail.com>
2018-10-26 15:17:52 -0500

    Changes to scratchwork
    Went a few layers deeper into the dicom image. Still need to figure out how to loop through each image and save all results to some sort of array.

--- scratchwork.m
+++ scratchwork.m
@@ -14,17 +14,36 @@ for inum=1:nfiles
 end
 %images array now contains the entire dataset of dicom files
 
-sX=size(images{1});
+sX=size(images{5});
 
 %Splits image into 4 layers
-[cA,cH,cV,cD]=dwt2(images{1},'db1');
+[LL,LH,HL,HH]=dwt2(images{5},'haar');
+[LL2, LH2, HL2 ,HH2] = dwt2(LL, 'haar');
+[LL3, LH3, HL3, HH3] = dwt2(LL2, 'haar');
+[LL4, LH4, HL4, HH4] = dwt2(LL3, 'haar');
+[LL5, LH5, HL5, HH5] = dwt2(LL4, 'haar');
 
 figure(1)
-subplot(2,2,1);imshow(cA);title('cA band of image');
-subplot(2,2,2);imshow(cH);title('cH band of image');
-subplot(2,2,3);imshow(cV);title('cV band of image');
-subplot(2,2,4);imshow(cD);title('cD band of image');
-X = idwt2(cA,cH,cV,cD,'db1',sX);
+subplot(5,4,1);imshow(LL);title('LL band of image');
+subplot(5,4,2);imshow(LH);title('LH band of image');
+subplot(5,4,3);imshow(HL);title('HL band of image');
+subplot(5,4,4);imshow(HH);title('HH band of image');
+subplot(5,4,5);imshow(LL2);title('LL2 band of image');
+subplot(5,4,6);imshow(LH2);title('LH2 band of image');
+subplot(5,4,7);imshow(HL2);title('HL2 band of image');
+subplot(5,4,8);imshow(HH2);title('HH2 band of image');
+subplot(5,4,9);imshow(LL3);title('LL3 band of image');
+subplot(5,4,10);imshow(LH3);title('LH3 band of image');
+subplot(5,4,11);imshow(HL3);title('HL3 band of image');
+subplot(5,4,12);imshow(HH3);title('HH3 band of image');
+subplot(5,4,13);imshow(LL4);title('LL4 band of image');
+subplot(5,4,14);imshow(LH4);title('LH4 band of image');
+subplot(5,4,15);imshow(HL4);title('HL4 band of image');
+subplot(5,4,16);imshow(HH4);title('HH4 band of image');
+subplot(5,4,17);imshow(LL5);title('LL5 band of image');
+subplot(5,4,18);imshow(LH5);title('LH5 band of image');
+subplot(5,4,19);imshow(HL5);title('HL5 band of image');
+subplot(5,4,20);imshow(HH5);title('HH5 band of image');
+X = idwt2(LL,LH,HL,HH,'haar',sX);
 
-figure(2)
 imshow(X);
